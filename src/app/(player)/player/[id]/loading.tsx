5f307136fa2a7a97fd84ebeb68b7038d3a2a0d0c
Tomas Nasjleti <acatomas@gmail.com>
2023-10-16 17:39:41 +0200

    improved loading state

--- src/app/(player)/player/[id]/loading.tsx
+++ src/app/(player)/player/[id]/loading.tsx
@@ -7,24 +7,10 @@ export default function PlayerLoading() {
       <div className="container">
         <Navbar />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-[1fr_20%] border h-[80vh]">
+      <div className="grid grid-cols-1 border h-[80vh]">
         <div className="w-full h-full">
           <Skeleton className="w-full h-full" />
         </div>
-        <div className="p-4">
-          <Skeleton className="w-full h-12" />
-          <div className="mt-4">
-            <Skeleton className="w-full h-8 mt-4" />
-            <Skeleton className="w-full h-8 mt-4" />
-            <Skeleton className="w-full h-8 mt-4" />
-            <Skeleton className="w-full h-8 mt-4" />
-          </div>
-        </div>
-      </div>
-      <div className="container flex gap-4">
-        <Skeleton className="h-12 mt-2 w-1/3" />
-        <Skeleton className="h-12 mt-2 w-1/3" />
-        <Skeleton className="h-12 mt-2 w-1/3" />
       </div>
     </>
   );
