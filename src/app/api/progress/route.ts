f98caaf08cd64a62226785df5d2ef565da0af031
Tomas Nasjleti <acatomas@gmail.com>
2023-10-16 17:31:53 +0200

    route error handlers

--- src/app/api/progress/route.ts
+++ src/app/api/progress/route.ts
@@ -4,59 +4,71 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const { searchParams } = new URL(req.url);
+  try {
+    const { searchParams } = new URL(req.url);
 
-  const videoId = searchParams.get("videoId") ?? "";
+    const videoId = searchParams.get("videoId") ?? "";
 
-  const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions);
 
-  const userVideo = await db.video.findUnique({
-    where: {
-      id: videoId,
-      userId: session?.user.id,
-    },
-  });
+    const userVideo = await db.video.findUnique({
+      where: {
+        id: videoId,
+        userId: session?.user.id,
+      },
+    });
 
-  const videoChapters = await db.chapter.findMany({
-    where: {
-      videoId: userVideo?.id,
-    },
-    orderBy: {
-      startTime: "asc",
-    },
-  });
-  return NextResponse.json({
-    ...userVideo,
-    chapters: videoChapters,
-  });
+    const videoChapters = await db.chapter.findMany({
+      where: {
+        videoId: userVideo?.id,
+      },
+      orderBy: {
+        startTime: "asc",
+      },
+    });
+    return NextResponse.json({
+      ...userVideo,
+      chapters: videoChapters,
+    });
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
 
 export async function PATCH(req: NextRequest) {
-  const { chapterId } = await req.json();
+  try {
+    const { chapterId } = await req.json();
 
-  const mutation = await db.chapter.update({
-    where: {
-      id: chapterId,
-    },
-    data: {
-      completed: true,
-    },
-  });
+    const mutation = await db.chapter.update({
+      where: {
+        id: chapterId,
+      },
+      data: {
+        completed: true,
+      },
+    });
 
-  return NextResponse.json({ data: mutation });
+    return NextResponse.json({ data: mutation });
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
 
 export async function PUT(req: NextRequest) {
-  const { videoId } = await req.json();
+  try {
+    const { videoId } = await req.json();
 
-  const mutation = await db.chapter.updateMany({
-    where: {
-      videoId,
-    },
-    data: {
-      completed: false,
-    },
-  });
+    const mutation = await db.chapter.updateMany({
+      where: {
+        videoId,
+      },
+      data: {
+        completed: false,
+      },
+    });
 
-  return NextResponse.json({ data: mutation });
+    return NextResponse.json({ data: mutation });
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,27 +3,31 @@ import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const session = await getServerSession();
-
-  const userVideos = await db.video.findMany({
-    where: {
-      userId: session?.user.id,
-    },
-  });
-
-  const userVideosWithChapters = await Promise.all(
-    userVideos.map(async (video) => {
-      const chapters = await db.chapter.findMany({
-        where: {
-          videoId: video.id,
-        },
-      });
-      return {
-        ...video,
-        chapters,
-      };
-    })
-  );
-
-  return NextResponse.json({ videos: userVideosWithChapters });
+  try {
+    const session = await getServerSession();
+  
+    const userVideos = await db.video.findMany({
+      where: {
+        userId: session?.user.id,
+      },
+    });
+  
+    const userVideosWithChapters = await Promise.all(
+      userVideos.map(async (video) => {
+        const chapters = await db.chapter.findMany({
+          where: {
+            videoId: video.id,
+          },
+        });
+        return {
+          ...video,
+          chapters,
+        };
+      })
+    );
+  
+    return NextResponse.json({ videos: userVideosWithChapters });
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
