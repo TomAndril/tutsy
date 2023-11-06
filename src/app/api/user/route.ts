cb6d460411c31efe98d3bd65179b80fea0453d03
Tomas Nasjleti <acatomas@gmail.com>
2023-11-06 23:16:22 +0100

    rollback to use force dynamic

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,28 +2,34 @@ import { db } from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
+export const dynamic = 'force-dynamic'
+
 export async function GET() {
-  const session = await getServerSession();
-
-  const userVideos = await db.video.findMany({
-    where: {
-      userId: session?.user.id,
-    },
-  });
-
-  const userVideosWithChapters = await Promise.all(
-    userVideos.map(async (video) => {
-      const chapters = await db.chapter.findMany({
-        where: {
-          videoId: video.id,
-        },
-      });
-      return {
-        ...video,
-        chapters,
-      };
-    })
-  );
-
-  return NextResponse.json({ videos: userVideosWithChapters });
+  try {
+    const session = await getServerSession();
+  
+    const userVideos = await db.video.findMany({
+      where: {
+        userId: session?.user.id,
+      },
+    });
+  
+    const userVideosWithChapters = await Promise.all(
+      userVideos.map(async (video) => {
+        const chapters = await db.chapter.findMany({
+          where: {
+            videoId: video.id,
+          },
+        });
+        return {
+          ...video,
+          chapters,
+        };
+      })
+    );
+  
+    return NextResponse.json({ videos: userVideosWithChapters });
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
