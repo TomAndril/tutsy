5436b30adf46b4300c2e5c887153fea9029890f4
Tomas Nasjleti <tomas.nasjleti@inter.ikea.com>
2024-02-02 00:43:35 -0300

    Fix import

--- src/hooks/use-get-user-config.tsx
+++ src/hooks/use-get-user-config.tsx
@@ -1,11 +1,11 @@
 import { QueryKeys } from "@/constants";
-import { getUserConfiguration } from "@/lib/user";
+import { getPlayerConfig } from "@/lib/user";
 import { useQuery } from "@tanstack/react-query";
 import { cookies } from "next/headers";
 
 export default function useGetUserConfig() {
   return useQuery({
-    queryKey: [QueryKeys.USER_CONFIG],
-    queryFn: () => getUserConfiguration(cookies()),
+    queryKey: [QueryKeys.PLAYER_CONFIG],
+    queryFn: () => getPlayerConfig(cookies()),
   });
 }
