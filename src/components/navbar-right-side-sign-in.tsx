f478231677c388e53c5388ad2652f734be29e9be
Tomas Nasjleti <tomas.nasjleti@inter.ikea.com>
2024-02-10 21:28:45 -0300

    Update sign-in button size

--- src/components/navbar-right-side-sign-in.tsx
+++ src/components/navbar-right-side-sign-in.tsx
@@ -6,7 +6,7 @@ import { signIn } from "next-auth/react";
 export default function NavBarRightSideSignIn() {
   return (
     <div>
-      <Button onClick={() => signIn()}>Sign In</Button>
+      <Button size="sm" onClick={() => signIn()}>Sign In</Button>
     </div>
   );
 }
