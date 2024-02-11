913e56583f147c19b7a9c1b5eb78e97b3f1dae4c
Tomas Nasjleti <tomas.nasjleti@inter.ikea.com>
2024-02-10 21:14:48 -0300

    Added grid background to hero

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,13 +1,14 @@
+import GridBackground from "@/components/grid-background";
 import Hero from "@/components/hero";
 
 export default function IndexPage() {
   return (
-    <div className="h-screen md:h-[70vh] w-full">
+    <GridBackground>
       <main className="container">
         <div className="text-left">
           <Hero />
         </div>
       </main>
-    </div>
+    </GridBackground>
   );
 }
