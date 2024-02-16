00bafc7173e96ca7dbe0dfa4eb735ae53c8fee6b
Tomas Nasjleti <tomas.nasjleti@inter.ikea.com>
2024-02-16 10:05:22 -0300

    Tiny CSS fix

--- src/components/player-video-data.tsx
+++ src/components/player-video-data.tsx
@@ -4,7 +4,7 @@ import { Icons } from "./icons";
 export default function PlayerVideoData(data: VideoWithChapters) {
   return (
     <div className="container mt-4">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-between flex-wrap gap-2">
         <div>
           <h1 className="text-sm md:text-base font-semibold">{data.title}</h1>
           <p className="text-xs md:text-sm text-slate-400 font-semibold">
@@ -16,7 +16,7 @@ export default function PlayerVideoData(data: VideoWithChapters) {
           target="_blank"
           className="text-xs flex items-center hover:underline"
         >
-          Open in YouTube <Icons.extenalLink size={14} className="ml-2" />
+          Open in YouTube <Icons.extenalLink size={14} className="ml-1" />
         </a>
       </div>
     </div>
