52e95d0d70c567e5258daa35efad3745ee1cafdd
Tomas Nasjleti <tomas.nasjleti@inter.ikea.com>
2024-02-18 12:22:32 -0300

    Refactor VideoPlayer logic

--- src/components/youtube-video-player.tsx
+++ src/components/youtube-video-player.tsx
@@ -2,141 +2,45 @@ import "../styles/player.css";
 import ReactPlayer from "react-player";
 import { VideoWithChapters } from "@/types/video";
 import { cn } from "@/lib/utils";
-import { useCallback, useRef, useState } from "react";
-import useUpdateChapterStatus from "@/hooks/use-update-chapter-status";
-import { Chapter, Config } from "@prisma/client";
-import { toast } from "./ui/use-toast";
-import { ToastAction } from "./ui/toast";
+import { useRef } from "react";
+import { Config } from "@prisma/client";
 import ChapterSelector from "./chapter-selector";
+import useYoutubeVideoPlayer from "@/hooks/use-youtube-video-player";
 interface Props {
   video: VideoWithChapters;
   userConfig: Config;
 }
 
-const hasCompletedAllChapters = (
-  chapters: Chapter[],
-  currentChapterIndex: number
-) => {
-  chapters[currentChapterIndex].completed = true;
-  return chapters.every((c) => c.completed);
+const chapterSelectorVariables = {
+  id: "",
+  title: "",
+  startTime: 0,
+  videoId: null,
+  completed: false,
+  completedAt: null,
 };
 
-// TODO: Cleanup this file as it's a mess. Maybe split it into multiple
-// components or extract some logic into hooks
+const REACT_PLAYER_CONFIG = {
+  file: {
+    attributes: {
+      controlsList: "nodownload",
+    },
+  },
+};
 
 export default function YoutubeVideoPlayer({ video, userConfig }: Props) {
-  const [isVideoReady, setIsVideoReady] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [currentChapter, setCurrentChapter] = useState<Chapter>();
-
-  const { mutate, isPending, variables } = useUpdateChapterStatus(video);
-
   const playerRef = useRef<ReactPlayer>(null);
 
-  const hasChapters = video.chapters?.length > 0;
-
-  const handleJumpToChapter = useCallback((ms: number) => {
-    playerRef.current?.seekTo(ms);
-    setIsPlaying(true);
-  }, []);
-
-  const handleUpdateChapter = useCallback(
-    (playedSeconds: number) => {
-      const currentChapterIndex =
-        video.chapters.findIndex((c) => c.startTime >= playedSeconds) - 1;
-
-      const isFinalChapter = hasChapters
-        ? playedSeconds >= video.chapters[video.chapters.length - 1].startTime
-        : false;
-
-      setCurrentChapter(
-        video.chapters[
-          isFinalChapter ? video.chapters.length - 1 : currentChapterIndex
-        ]
-      );
-
-      // If the video is almost finished, mark the last chapter as completed
-      if (
-        video.duration - playedSeconds < 3 &&
-        !isPending &&
-        !video.chapters[video.chapters.length - 1]?.completed
-      ) {
-        return mutate({
-          chapter: video.chapters[video.chapters.length - 1],
-          hasCompletedAllChapters: hasCompletedAllChapters(
-            video.chapters,
-            video.chapters.length - 1
-          ),
-        });
-      }
-
-      const nextChapterStartTime =
-        video.chapters?.[currentChapterIndex + 1]?.startTime;
-
-      const hasToUpdateChapter =
-        !video.chapters[currentChapterIndex]?.completed &&
-        nextChapterStartTime - playedSeconds < 3 &&
-        playedSeconds > 5;
-
-      if (hasToUpdateChapter && !isPending) {
-        mutate({
-          chapter: video.chapters[currentChapterIndex],
-          hasCompletedAllChapters: hasCompletedAllChapters(
-            video.chapters,
-            currentChapterIndex
-          ),
-        });
-      }
-    },
-    [video.chapters, video.duration, isPending, mutate, hasChapters]
-  );
-
-  const handleOnReady = () => {
-    if (isVideoReady) {
-      return;
-    }
-
-    if (userConfig === null || userConfig.jumpToLastChapter) {
-      const hasLastViewedChapter = video.chapters.some((c) => c.completedAt);
-
-      if (hasLastViewedChapter) {
-        const lastViewedChapter = video.chapters
-          .filter((c) => c.completedAt)
-          .sort((a, b) => {
-            if (a.completedAt && b.completedAt) {
-              return a.completedAt < b.completedAt ? 1 : -1;
-            }
-            return 0;
-          });
-
-        handleJumpToChapter(lastViewedChapter[0].startTime);
-        setIsVideoReady(true);
-
-        if (localStorage.getItem("jumpToLastChapterToast") !== "false") {
-          toast({
-            title: "Jumping to last viewed chapter",
-            variant: "default",
-            description: "You can disable this behavior in settings",
-            action: (
-              <ToastAction
-                onClick={() =>
-                  localStorage.setItem("jumpToLastChapterToast", "false")
-                }
-                className="text-xs"
-                altText="Don't show this again"
-              >
-                Don&apos;t show <br /> this again
-              </ToastAction>
-            ),
-          });
-        }
-
-        return;
-      }
-    }
-    setIsVideoReady(true);
-    setIsPlaying(true);
-  };
+  const {
+    currentChapter,
+    hasChapters,
+    isPlaying,
+    isVideoReady,
+    isPending,
+    handleUpdateChapter,
+    handleOnReady,
+    handleJumpToChapter,
+  } = useYoutubeVideoPlayer(video, userConfig, playerRef);
 
   return (
     <div
@@ -160,26 +64,13 @@ export default function YoutubeVideoPlayer({ video, userConfig }: Props) {
           height={"100%"}
           controls
           url={`https://invidious.fdn.fr/latest_version?id=${video.youtubeId}`}
-          config={{
-            file: {
-              attributes: {
-                controlsList: "nodownload",
-              },
-            },
-          }}
+          config={REACT_PLAYER_CONFIG}
           playsinline
         />
       </div>
       <ChapterSelector
         video={video}
-        variables={{
-          id: "",
-          title: "",
-          startTime: 0,
-          videoId: null,
-          completed: false,
-          completedAt: null,
-        }}
+        variables={chapterSelectorVariables}
         currentChapter={currentChapter}
         handleJumpToChapter={handleJumpToChapter}
         isPending={isPending}
---
+++ src/hooks/use-youtube-video-player.tsx
@@ -0,0 +1,146 @@
+import { VideoWithChapters } from "@/types/video";
+import { Chapter, Config } from "@prisma/client";
+import { useCallback, useRef, useState } from "react";
+import ReactPlayer from "react-player";
+import useUpdateChapterStatus from "./use-update-chapter-status";
+import { toast } from "@/components/ui/use-toast";
+import { ToastAction } from "@/components/ui/toast";
+
+const hasCompletedAllChapters = (
+  chapters: Chapter[],
+  currentChapterIndex: number
+) => {
+  chapters[currentChapterIndex].completed = true;
+  return chapters.every((c) => c.completed);
+};
+
+export default function useYoutubeVideoPlayer(
+  video: VideoWithChapters,
+  userConfig: Config,
+  playerRef: React.RefObject<ReactPlayer>
+) {
+  const [isVideoReady, setIsVideoReady] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentChapter, setCurrentChapter] = useState<Chapter>();
+
+  const { mutate, isPending } = useUpdateChapterStatus(video);
+
+  const hasChapters = video.chapters?.length > 0;
+
+  const handleJumpToChapter = useCallback(
+    (ms: number) => {
+      playerRef.current?.seekTo(ms);
+      setIsPlaying(true);
+    },
+    [playerRef]
+  );
+
+  const handleUpdateChapter = useCallback(
+    (playedSeconds: number) => {
+      const currentChapterIndex =
+        video.chapters.findIndex((c) => c.startTime >= playedSeconds) - 1;
+
+      const isFinalChapter = hasChapters
+        ? playedSeconds >= video.chapters[video.chapters.length - 1].startTime
+        : false;
+
+      setCurrentChapter(
+        video.chapters[
+          isFinalChapter ? video.chapters.length - 1 : currentChapterIndex
+        ]
+      );
+
+      // If the video is almost finished, mark the last chapter as completed
+      if (
+        video.duration - playedSeconds < 3 &&
+        !isPending &&
+        !video.chapters[video.chapters.length - 1]?.completed
+      ) {
+        return mutate({
+          chapter: video.chapters[video.chapters.length - 1],
+          hasCompletedAllChapters: hasCompletedAllChapters(
+            video.chapters,
+            video.chapters.length - 1
+          ),
+        });
+      }
+
+      const nextChapterStartTime =
+        video.chapters?.[currentChapterIndex + 1]?.startTime;
+
+      const hasToUpdateChapter =
+        !video.chapters[currentChapterIndex]?.completed &&
+        nextChapterStartTime - playedSeconds < 3 &&
+        playedSeconds > 5;
+
+      if (hasToUpdateChapter && !isPending) {
+        mutate({
+          chapter: video.chapters[currentChapterIndex],
+          hasCompletedAllChapters: hasCompletedAllChapters(
+            video.chapters,
+            currentChapterIndex
+          ),
+        });
+      }
+    },
+    [video.chapters, video.duration, isPending, mutate, hasChapters]
+  );
+
+  const handleOnReady = () => {
+    if (isVideoReady) {
+      return;
+    }
+
+    if (userConfig === null || userConfig.jumpToLastChapter) {
+      const hasLastViewedChapter = video.chapters.some((c) => c.completedAt);
+
+      if (hasLastViewedChapter) {
+        const lastViewedChapter = video.chapters
+          .filter((c) => c.completedAt)
+          .sort((a, b) => {
+            if (a.completedAt && b.completedAt) {
+              return a.completedAt < b.completedAt ? 1 : -1;
+            }
+            return 0;
+          });
+
+        handleJumpToChapter(lastViewedChapter[0].startTime);
+        setIsVideoReady(true);
+
+        if (localStorage.getItem("jumpToLastChapterToast") !== "false") {
+          toast({
+            title: "Jumping to last viewed chapter",
+            variant: "default",
+            description: "You can disable this behavior in settings",
+            action: (
+              <ToastAction
+                onClick={() =>
+                  localStorage.setItem("jumpToLastChapterToast", "false")
+                }
+                className="text-xs"
+                altText="Don't show this again"
+              >
+                Don&apos;t show <br /> this again
+              </ToastAction>
+            ),
+          });
+        }
+
+        return;
+      }
+    }
+    setIsVideoReady(true);
+    setIsPlaying(true);
+  };
+
+  return {
+    isVideoReady,
+    isPlaying,
+    isPending,
+    currentChapter,
+    hasChapters,
+    handleJumpToChapter,
+    handleUpdateChapter,
+    handleOnReady,
+  };
+}
