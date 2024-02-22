7f3409699ba1392b508f30b2ae40cbf0e5134473
Tomas Nasjleti <tomas.nasjleti@inter.ikea.com>
2024-02-22 16:00:20 -0300

    Added error catch to search route

--- src/app/(marketing)/search/page.tsx
+++ src/app/(marketing)/search/page.tsx
@@ -1,14 +1,18 @@
 import SearchContainer from "@/components/search-container";
 import { getVideoSearchResults } from "@/lib/videos";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 export async function generateMetadata({
   searchParams,
 }: any): Promise<Metadata> {
   const { q } = searchParams;
+
+  const title = q ? `${q} | search on tutsy` : 'Tutsy | Not Found';
+
   return {
-    title: `${q} | search on tutsy`,
-    description: "YouTube Learning, reinvented."
+    title,
+    description: "YouTube Learning, reinvented.",
   };
 }
 
@@ -17,7 +21,11 @@ export default async function SearchPage({
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const query = searchParams?.q;
+  const query = searchParams?.q as string;
+
+  if (!query || query.trim().length === 0) {
+    return notFound();
+  }
 
   const initialData = await getVideoSearchResults(query as string);
 
