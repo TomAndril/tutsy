10c39c3b6eec9d4118e53095a94cf0fab04ad76f
Tomas Nasjleti <acatomas@gmail.com>
2024-02-25 13:14:54 +0100

    Refactor hero component and remove spotlight component

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,27 +2,23 @@
 
 import { RevealList } from "next-reveal";
 import NavBarSearch from "./navbar-search";
-import Spotlight from "./spotlight";
 
 export default function Hero() {
   return (
-    <>
-      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20 overflow-hidden" />
-      <RevealList interval={200} delay={100}>
-        <h1 className="load-hidden text-5xl md:text-7xl lg:text-9xl font-semibold tracking-tighter pt-16 text-center font-mono drop-shadow-lg bg-gradient-to-b from-black dark:from-white via-slate-900 dark:via-slate-100 to-slate-700 dark:to-slate-200 bg-clip-text text-transparent">
-          YouTube learning
-          <br />
-          <span className="font-black italic">Reinvented</span>
-        </h1>
-        <h2 className="load-hidden mt-8 text-center font-extralight text-base md:text-lg lg:text-xl max-w-2xl mx-auto">
-          Tutsy is a new way to learn from YouTube videos. It&apos;s a platform
-          that allows you to learn from the best content creators on YouTube,
-          without the distractions.
-        </h2>
-        <div className="load-hidden max-w-2xl mx-auto mt-12 flex">
-          <NavBarSearch isHero />
-        </div>
-      </RevealList>
-    </>
+    <RevealList interval={200} delay={100}>
+      <h1 className="load-hidden text-5xl md:text-7xl lg:text-9xl font-semibold tracking-tighter pt-16 text-center font-mono drop-shadow-lg bg-gradient-to-b from-black dark:from-white via-slate-900 dark:via-slate-100 to-slate-700 dark:to-slate-200 bg-clip-text text-transparent">
+        YouTube learning
+        <br />
+        <span className="font-black italic">Reinvented</span>
+      </h1>
+      <h2 className="load-hidden mt-8 text-center font-extralight text-base md:text-lg lg:text-xl max-w-2xl mx-auto">
+        Tutsy is a new way to learn from YouTube videos. It&apos;s a platform
+        that allows you to learn from the best content creators on YouTube,
+        without the distractions.
+      </h2>
+      <div className="load-hidden max-w-2xl mx-auto mt-12 flex">
+        <NavBarSearch isHero />
+      </div>
+    </RevealList>
   );
 }
