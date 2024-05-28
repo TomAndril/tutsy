71bff381a482dd1976a64d7db82a8b56c5fde678
Tomas Nasjleti <acatomas@gmail.com>
2024-05-28 13:53:07 +0200

    new try

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,5 +2,9 @@ export { auth as middleware } from "@/lib/auth";
 
 // Routes that should be protected
 export const config = {
-  matcher: ["/player/(.*)", "/dashboard/(.*)"],
+  matcher: [
+    "/player/(.*)",
+    "/dashboard/(.*)",
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  ],
 };
